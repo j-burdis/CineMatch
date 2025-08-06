ac1b759ca9227e580c614c6291b8551925f9a302
catherine <catherine.janssen@yahoo.com>
2025-08-06 15:59:13 +0100

    render hex codes as coloured cubes

--- cinematch/src/main/java/com/cinematch/cinematch/controller/ColourController.java
+++ cinematch/src/main/java/com/cinematch/cinematch/controller/ColourController.java
@@ -1,10 +1,12 @@
 package com.cinematch.cinematch.controller;
 
+import ch.qos.logback.core.model.Model;
 import com.cinematch.cinematch.service.ColourService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -18,8 +20,16 @@ public class ColourController {
         this.colourService = colourService;
     }
 
-    @GetMapping("/colours")
-    public List<String> ColourPalette(@PathVariable String hex) {
-        return colourService.getColours(hex);
+    //route for colour api request
+    @GetMapping("/colours/{hex}")
+    public ModelAndView ColourPalette(@PathVariable String hex) {
+        List<String> ColoursArray = colourService.getColours(hex);
+
+        //thymeleaf connection
+        ModelAndView modelAndView = new ModelAndView("colour-palette");
+        modelAndView.addObject("coloursArray", ColoursArray);
+        System.out.println(modelAndView);
+        return modelAndView;
+
     }
 }
