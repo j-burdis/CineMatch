63662d4e4d850b33070356def86ee9b2a7496527
jon <jonnyburdis@hotmail.com>
2025-08-06 15:15:54 +0100

    hold api keys in .env file

--- cinematch/src/main/java/com/cinematch/cinematch/service/MovieService.java
+++ cinematch/src/main/java/com/cinematch/cinematch/service/MovieService.java
@@ -4,6 +4,7 @@ import com.cinematch.cinematch.model.Movie;
 import com.cinematch.cinematch.model.MovieResponse;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,13 +13,17 @@ import java.util.List;
 @Service
 public class MovieService {
 
-    @Value("${tmdb.api.key}")
-    private String apiKey;
+    private final String apiKey;
+
+    public MovieService() {
+        Dotenv dotenv = Dotenv.load();
+        this.apiKey = dotenv.get("TMDB_API_KEY");
+    }
 
     public List<Movie> getPopularMovies() {
-        String URL = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey;
+        String url = "https://api.themoviedb.org/3/movie/popular?api_key=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
-        MovieResponse response = restTemplate.getForObject(URL, MovieResponse.class);
+        MovieResponse response = restTemplate.getForObject(url, MovieResponse.class);
         return response != null ? response.getResults() : List.of();
     }
 }
