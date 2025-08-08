2a5975cd3eda6658520057a87cbdef8fd71b1256
jon <jonnyburdis@hotmail.com>
2025-08-08 16:01:14 +0100

    no duplicates in paint matches and colour palette tables

--- cinematch/src/main/java/com/cinematch/cinematch/repository/ColourRepository.java
+++ cinematch/src/main/java/com/cinematch/cinematch/repository/ColourRepository.java
@@ -1,8 +1,14 @@
 package com.cinematch.cinematch.repository;
 
 import com.cinematch.cinematch.model.ColourModel;
+import com.cinematch.cinematch.model.Movie;
+import com.cinematch.cinematch.model.PaintMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ColourRepository extends JpaRepository<ColourModel, String> {
+    Optional<ColourModel> findByMovie(Movie movie);
+    boolean existsByMovie(Movie movie);
 }
--- cinematch/src/main/java/com/cinematch/cinematch/repository/DuluxColourRepository.java
+++ cinematch/src/main/java/com/cinematch/cinematch/repository/DuluxColourRepository.java
@@ -3,6 +3,10 @@ package com.cinematch.cinematch.repository;
 import com.cinematch.cinematch.model.DuluxColour;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DuluxColourRepository extends JpaRepository<DuluxColour, Long> {
     boolean existsByHexCode(String hexCode);
+
+    Optional<DuluxColour> findByHexCode(String hex);
 }
