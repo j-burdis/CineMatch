8513ebbd9b82096078a8d2942664c67e94caf5b4
jon <jonnyburdis@hotmail.com>
2025-08-11 14:02:10 +0100

    add search feature

--- cinematch/src/main/java/com/cinematch/cinematch/controller/MovieController.java
+++ cinematch/src/main/java/com/cinematch/cinematch/controller/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -28,4 +29,11 @@ public class MovieController {
     public ModelAndView showMovieDetails(@PathVariable Long id) {
         return movieDetailsService.buildMovieDetail(id);
     }
+
+    @GetMapping("/movies/search")
+    public String searchMovies(@RequestParam("q") String query, Model model) {
+        model.addAttribute("movies", movieService.searchMovies(query));
+        return "movies";
+    }
+
 }
